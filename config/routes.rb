632b27c4a489320c0d2e85459d02c585f2ebae2d
Dagdagger <omar.brt939@gmail.com>
2017-10-25 15:00:13 -0700

    Added a coach user signup page, TODO: Stylize the pages make it obvious which one is which

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
-  get 'trainers/index'
 
-  get 'trainer/index'
+mount ActionCable.server => '/cable'
+root 'home#index'
+devise_for :users
+devise_for :coaches, path: 'coaches', controllers:
+{sessions: "coaches/sessions"}
 
+authenticated :user do
   resources :charges
-
-  get 'trainer/index'
   resources :chat_rooms, only: [:new, :create, :show, :index]
-  mount ActionCable.server => '/cable'
-  resources :posts
-  resources :trainers
-  devise_for :users
-  root 'home#index'
-  post 'twilio/voice' => 'twilio#voice'
-  devise_for :models
   get 'events/search' => 'events#search'
   get  'events/hello'  => 'events#hello'
   get 'profiles/:id' => 'profiles#show'
@@ -21,5 +16,7 @@ Rails.application.routes.draw do
   resources :events
   resources :users
   resources :profiles
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
